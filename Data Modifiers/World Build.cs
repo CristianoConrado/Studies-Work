1d8bd96de9c888cc05a8ae82471f2ce4991113d2
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-11-29 03:19:55 -0300

    Add files via upload

---
+++ Data Modifiers/World Build.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+namespace StudiesWork.DataModifiers
+{
+	public class WorldBuild
+	{
+		public const short STRAIGTH_COST = 10;
+		public const short DIAGONAL_COST = 14;
+		private enum Layers
+		{
+			Scene,
+			Target
+		};
+		public static readonly LayerMask SceneMask = GetMask(Layers.Scene);
+		public static readonly LayerMask TargetMask = GetMask(Layers.Target);
+		private static LayerMask GetMask(Layers layerName)
+		{
+			for(int i = 0; i < 32; i++)
+				if(LayerMask.LayerToName(i) != string.Empty && LayerMask.LayerToName(i).ToUpper() == layerName.ToString().ToUpper())
+					return 1 << i;
+			return 0;
+		}
+	};
+};
